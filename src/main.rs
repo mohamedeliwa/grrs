51bf06846d66b9b74ca69fead88b45e206b706dc
mohamedeliwa <mohamed.av11@gmail.com>
2023-05-06 16:04:10 +0300

    reading file and extracting the lines that contains the pattern

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
+use std::fs;
+use std::io::{self, BufRead};
 
 /// Search for a pattern in a file and display the lines that contain it.
-#[derive(Debug)]
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 struct Cli {
     /// The pattern to look for
     pattern: String,
@@ -15,6 +16,13 @@ struct Cli {
 fn main() {
     // the arguments that this program was started with (normally passed via the command line).
     // return an iterator
-    let args =  Cli::parse();
+    let args = Cli::parse();
+    
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
