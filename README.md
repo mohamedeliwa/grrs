d51de1a3ebcfa95636ef72991b8a3dee7b8c7e6c
mohamedeliwa <mohamed.av11@gmail.com>
2023-05-06 10:36:27 +0300

    CLI arguments as data type

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # grrs
 
 a small grep clone (pronounced “grass”). built using rust "for learning purposes"
+
+a typical invocation to `grrs`, should look like this
+
+`$ grrs foobar test.txt`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
+#[derive(Debug)]
+struct Cli {
+    pattern: String,
+    // PathBuf is like a String but for file system paths that work cross-platform.
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // the arguments that this program was started with (normally passed via the command line).
+    // return an iterator
+    let mut args = env::args();
+    
+    let pattern = args.nth(1).expect("no pattern given");
+    let path = args.nth(2).expect("no path given");
+
 }
