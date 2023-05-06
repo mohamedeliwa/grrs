e745c4f8f59faae1d92ce86c36eb67a470b87d49
mohamedeliwa <mohamed.av11@gmail.com>
2023-05-06 11:03:29 +0300

    using clap crate to parse cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use std::env;
+use clap::Parser;
 
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Debug)]
+#[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+
     // PathBuf is like a String but for file system paths that work cross-platform.
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
 
 fn main() {
     // the arguments that this program was started with (normally passed via the command line).
     // return an iterator
-    let mut args = env::args();
-    
-    let pattern = args.nth(1).expect("no pattern given");
-    let path = args.nth(2).expect("no path given");
+    let args =  Cli::parse();
 
 }
