94f1b4a39a90c4788e3f48f44dffbb8b00d1f905
mohamedeliwa <mohamed.av11@gmail.com>
2023-05-11 20:44:11 +0300

    seperating small logic, and unit testing it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use anyhow::{Context, Ok, Result};
+use clap::Parser;
 use std::fs;
 use std::io::{self, BufRead};
-use anyhow::{Context, Result};
-use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser, Debug)]
@@ -18,17 +18,32 @@ fn main() -> Result<()> {
     // the arguments that this program was started with (normally passed via the command line).
     // return an iterator
     let args = Cli::parse();
-    let file = fs::File::open(&args.path).with_context(|| format!("could not open file `{:?}`", args.path))?;
+    let file = fs::File::open(&args.path)
+        .with_context(|| format!("could not open file `{:?}`", args.path))?;
     let mut reader = io::BufReader::new(file);
     loop {
         let mut line = String::new();
-        let length = reader.read_line(&mut line).with_context(|| format!("could not read file `{:?}`", args.path))?;
+        let length = reader
+            .read_line(&mut line)
+            .with_context(|| format!("could not read file `{:?}`", args.path))?;
         if length == 0 {
             break;
         }
-        if line.contains(&args.pattern) {
-            println!("{line}");
-        }
+        match_a_line(&args.pattern, &line, &mut io::stdout())?;
     }
     Ok(())
+}
+
+fn match_a_line(pattern: &str, line: &str, writer: &mut impl std::io::Write) -> Result<()> {
+    if line.contains(pattern) {
+        writeln!(writer, "{line}").with_context(|| format!("could not write a match"))?;
+    }
+    Ok(())
+}
+
+#[test]
+fn should_match_a_line() {
+    let mut result: Vec<u8> = Vec::new();
+    match_a_line("lorem", "lorem ipsum\n", &mut result).unwrap();
+    assert_eq!(result, b"lorem ipsum\n\n");
 }
